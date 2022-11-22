66c0c633e6189b90b43565d918cf6b5b5b6a6e22
Oksana Babychevska <oksana.babychevska@isr.de>
2022-11-22 21:17:05 +0100

    Add section Brand

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
 import './brand.css';
+import { google, slack, attlasian, shopify, dropbox } from './imports.js'
 
 const Brand = () => {
   return (
-    <div>Brand</div>
+    <div className='gms__brand section__padding'>
+          <div>
+            <img src={google} alt="google" />
+          </div>
+          <div>
+            <img src={slack} alt="slack" />
+          </div>
+          <div>
+            <img src={attlasian} alt="attlasian" />
+          </div>
+          <div>
+            <img src={dropbox} alt="dropbox" />
+          </div>
+          <div>
+            <img src={shopify} alt="shopify" />
+          </div>
+    </div>
   )
 }
 
