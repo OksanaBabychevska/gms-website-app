470a1cfeafd48a5ea5cad91359634f77ae02fb1a
Oksana Babychevska <oksana.babychevska@isr.de>
2022-12-05 15:21:18 +0100

    Section cta

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -3,7 +3,15 @@ import './cta.css';
 
 const CTA = () => {
   return (
-    <div>CTA</div>
+    <div className='gms__cta section__margin'>
+      <div className="gms__cta-content">
+        <p>Request Early Access to Get Started</p>
+        <h3>Register today & start exploring the endless possiblities.</h3>
+      </div>
+      <div className="gms__cta-button">
+        <button type="button">Get Started</button>
+      </div>
+    </div>
   )
 }
 
