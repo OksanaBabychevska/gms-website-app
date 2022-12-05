9c4a639c30cce67663b7a62a6f4284fa02720412
Oksana Babychevska <oksana.babychevska@isr.de>
2022-12-05 19:31:54 +0100

    Add articles component and Blog section

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import './article.css';
 
-const Article = () => {
+const Article = ({ imgUrl, altAttr, date, title }) => {
   return (
-    <div>Article</div>
+    <div className='gms__article-container'>
+      <div className="gms__article-container-image">
+        <img src={imgUrl} alt={altAttr}/>
+      </div>
+      <div className="gms__article-container-text">
+        <p>{date}</p>
+        <h4>{title}</h4>
+        <p>Read Full Article</p>
+      </div>
+    </div>
   )
 }
 
--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
 import './blog.css';
+import Article from '../../components/article/Article';
+import { blog01, blog02, blog03, blog04, blog05 } from './imports';
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <div className='gms__blog section__padding' id="blog">
+      <div className="gms__blog-heading">
+        <h3 class='gradient__text'>A lot is happening,<br />
+         We are blogging about it.</h3>
+      </div>
+      <div className="gms__blog-container">
+        <div className="gms__blog-container_groupA">
+          <Article imgUrl={blog01} altAttr='blog image' date='Dec, 5, 2022' title ='GPT-3 and Open  AI is the future. Let us exlore how it is?'></Article>
+        </div>
+        <div className="gms__blog-container_groupB">
+          <Article imgUrl={blog02} altAttr='blog image' date='Dec, 5, 2022' title ='GPT-3 and Open  AI is the future. Let us exlore how it is?'></Article>
+          <Article imgUrl={blog03} altAttr='blog image' date='Dec, 5, 2022' title ='GPT-3 and Open  AI is the future. Let us exlore how it is?'></Article>
+          <Article imgUrl={blog04} altAttr='blog image' date='Dec, 5, 2022' title ='GPT-3 and Open  AI is the future. Let us exlore how it is?'></Article>
+          <Article imgUrl={blog05} altAttr='blog image' date='Dec, 5, 2022' title ='GPT-3 and Open  AI is the future. Let us exlore how it is?'></Article>
+        </div>
+      </div>
+    </div>
   )
 }
 
